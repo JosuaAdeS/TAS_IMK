9e6ee941e33ddccba441c3497ccc3678217510ed
Josua Ade S <672018272@student.uksw.edu>
2020-08-12 02:13:24 +0700

    Tiket User

--- js/vendor/foundation.js
+++ js/vendor/foundation.js
@@ -74,6 +74,28 @@ return /******/ (function(modules) { // webpackBootstrap
     });
 });
 
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var fiveMinutes = 60 * 5,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+};
 
 /******/ 	// expose the modules object (__webpack_modules__)
 /******/ 	__webpack_require__.m = modules;
